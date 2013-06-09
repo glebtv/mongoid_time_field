f17db29b40fe759cecd42677f908bebc41039635
glebtv <glebtv@gmail.com>
2013-06-09 10:27:39 +0400

    Add RailsAdmin support

--- lib/mongoid_time_field.rb
+++ lib/mongoid_time_field.rb
@@ -8,3 +8,39 @@ module Mongoid
     autoload :Value,        'mongoid_time_field/value'
   end
 end
+
+if Object.const_defined?("RailsAdmin")
+  require 'rails_admin/adapters/mongoid'
+  require 'rails_admin/config/fields/types/text'
+  module RailsAdmin
+    module Adapters
+      module Mongoid
+        alias_method :type_lookup_without_time_field, :type_lookup
+        def type_lookup(name, field)
+          if field.type.to_s == 'TimeField'
+            { :type => :time_field }
+          else
+            type_lookup_without_time_field(name, field)
+          end
+        end
+      end
+    end
+
+    module Config
+      module Fields
+        module Types
+          class TimeField < RailsAdmin::Config::Fields::Types::Text
+            # Register field type for the type loader
+            RailsAdmin::Config::Fields::Types::register(self)
+
+            register_instance_option :formatted_value do
+              p value
+              value.to_s
+            end
+          end
+        end
+      end
+    end
+  end
+
+end
